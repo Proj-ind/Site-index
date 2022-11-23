cebf27ea867d691a1f1ef8ed42cfcf2e3322b7c0
EnzoMedej <enzo.guimaraes@sptech.school>
2022-11-22 21:47:32 -0300

    mudanças dentro da tela index, na caixa de perguntas e na dash

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -10,41 +10,28 @@ CREATE DATABASE chromatica;
 
 USE chromatica;
 
+create table tribo(
+idTribo int primary key auto_increment,
+nome varchar(45)
+);
+
 CREATE TABLE usuario (
-	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	idUsuario INT primary key auto_increment,
 	nome VARCHAR(45),
 	sobrenome VARCHAR(45),
 	email VARCHAR(45),
 	senha VARCHAR(45)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table resposta(
+idresposta int,
+fkusuario int,
+foreign key(fkusuario) references usuario(idUsuario),
+fkTribo int,
+foreign key(fkTribo) references tribo(idTribo),
+primary key (idresposta, fkusuario, fktribo)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
 
 
 /*
