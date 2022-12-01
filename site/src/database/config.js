1bdda9e20287438fcdad4d30ba0e43dc937bbeb4
EnzomMedej <enzo.guimaraes@sptech.school>
2022-12-01 15:58:10 -0300

    Conclusão do css da page saiba mais

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "chromatica",
-    user: "root",
-    password: "31713",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
